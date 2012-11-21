9d2d9e5e813be8768d085776a73045ded09f1d1a
Xano <Xano@62965.no-reply.drupal.org>
2012-11-21 11:57:02 +0100

    Issue #1531974 by Xano: Make currencies Ctools exportables.

---
+++ currency.api.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * API documentation.
+ */
+
+/**
+ * Expose currencies.
+ *
+ * @return array
+ *   An array of Currency objects, keyed by their currency codes.
+ */
+function hook_currency_info() {
+  $currencies['EUR'] = new Currency(array(
+    'code' => 'EUR',
+    'minor_unit' => 2,
+    'sign' => '€',
+    'title' => t('Euro'),
+  ));
+
+  return $currencies;
+}
+
+/**
+ * Alter exposed currencies.
+ *
+ * @param array
+ *   An array of Currency objects, keyed by their currency codes.
+ *
+ * @return NULL
+ */
+function hook_currency_info_alter(array $currencies) {
+  // Let's pretend the euro has 1000 subunits.
+  $currencies['EUR']['minor_unit'] = 3;
+}
