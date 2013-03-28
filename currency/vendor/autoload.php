e48cdfbbe874a4661569b7cdce764fa94f50719e
Bart Feenstra <bart@mynameisbart.com>
2013-03-28 20:43:21 +0100

    Removed dependencies that are no longer required.

--- currency/vendor/autoload.php
+++ currency/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitbe80709f3273377ed05da1c63b578fbe::getLoader();
+return ComposerAutoloaderInitad12eb251983117123a0820d30e79c6e::getLoader();
--- currency/vendor/composer/autoload_namespaces.php
+++ currency/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Symfony\\Component\\Yaml' => $vendorDir . '/symfony/yaml/',
     'BartFeenstra' => array($vendorDir . '/bartfeenstra/cldr/src', $vendorDir . '/bartfeenstra/currency/src'),
 );
