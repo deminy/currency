92483e1c51468793d8b952bba26bfc2a642c6d76
Bart Feenstra <bart@mynameisbart.com>
2013-06-05 10:49:40 +0200

    Issue #2012376 by Xano: Convert input filters to plugins.

---
+++ currency/lib/Drupal/currency/Plugin/Filter/CurrencyExchange.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\currency\Plugin\Filter\CurrencyExchange.
+ */
+
+namespace Drupal\currency\Plugin\Filter;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\currency\Input;
+use Drupal\filter\Annotation\Filter;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to exchange currencies.
+ *
+ * @Filter(
+ *   id = "currency_exchange",
+ *   module = "currency",
+ *   title = @Translation("Currency exchange"),
+ *   type = FILTER_TYPE_MARKUP_LANGUAGE
+ * )
+ */
+class CurrencyExchange extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode, $cache, $cache_id) {
+    return preg_replace_callback('/\[currency:([a-z]{3}):([a-z]{3})(.*?)\]/i', array($this, 'processCallback'), $text);
+  }
+
+  /**
+   * Implements preg_replace_callback() callback.
+   *
+   * @see self::process()
+   */
+  function processCallback(array $matches) {
+    $currency_code_from = $matches[1];
+    $currency_code_to = $matches[2];
+    $amount = str_replace(':', '', $matches[3]);
+    if (strlen($amount) !== 0) {
+      $amount = Input::parseAmount($amount);
+      // The amount is invalid, so return the token.
+      if (!$amount) {
+        return $matches[0];
+      }
+    }
+    // The amount defaults to 1.
+    else {
+      $amount = 1;
+    }
+
+    $exchanger = \Drupal::service('currency.exchange_delegator');
+    $rate = $exchanger->load($currency_code_from, $currency_code_to);
+    if ($rate) {
+      return bcmul($amount, $rate, CURRENCY_BCMATH_SCALE);
+    }
+    // The filter failed, so return the token.
+    return $matches[0];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    return t('Use <code>[currency:from:to:amount]</code> to convert an amount of money from one currency to another. The <code>amount</code> parameter is optional and defaults to <code>1</code>. Example: <code>[currency:EUR:USD:100]</code>.');
+  }
+
+}
---
+++ currency/lib/Drupal/currency/Plugin/Filter/CurrencyLocalize.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\currency\Plugin\Filter\CurrencyExchange.
+ */
+
+namespace Drupal\currency\Plugin\Filter;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\currency\Input;
+use Drupal\filter\Annotation\Filter;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to format amounts.
+ *
+ * @Filter(
+ *   cache = false,
+ *   id = "currency_localize",
+ *   module = "currency",
+ *   title = @Translation("Currency amount formatting"),
+ *   type = FILTER_TYPE_MARKUP_LANGUAGE
+ * )
+ */
+class CurrencyLocalize extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode, $cache, $cache_id) {
+    return preg_replace_callback('/\[currency-localize:([a-z]{3}):(.+?)\]/i', array($this, 'processCallback'), $text);
+  }
+
+  /**
+   * Implements preg_replace_callback() callback.
+   *
+   * @see self::process()
+   */
+  function processCallback(array $matches) {
+    $currency_code = $matches[1];
+    $amount = Input::parseAmount($matches[2]);
+    // The amount is invalid, so return the token.
+    if (!$amount) {
+      return $matches[0];
+    }
+    $currency = entity_load('currency', $currency_code);
+    if ($currency) {
+      return $currency->format($amount);
+    }
+    // The currency code is invalid, so return the token.
+    return $matches[0];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    return t('Use <code>[currency-localize:<strong>currency-code</strong>:<strong>amount</strong>]</code> to localize an amount of money. The <code>currency-code</code> and <code>amount</code> parameters are the ISO 4217 currency code and the actual amount to display. Example: <code>[currency-localize:EUR:99.95]</code>.');
+  }
+
+}
