21ff7a5619f0e0d35f84cf23384f5771cf363e84
Bart Feenstra <bart@mynameisbart.com>
2013-07-11 11:09:46 +0200

    Issue #2039549 by Xano: Composer update.

--- currency/vendor/autoload.php
+++ currency/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0d1d23c4bb3f97e403b5b0395c4842f4::getLoader();
+return ComposerAutoloaderInitd6e79012324333065af2e5d0c2f07d03::getLoader();
