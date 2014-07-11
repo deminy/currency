9271df424b4b031ab39cfc52b9532fd0c997477b
Bart Feenstra <bart@mynameisbart.com>
2014-07-11 11:11:07 +0200

    Fixed an incorrect namespace declaration.

--- currency/tests/src/Entity/CurrencyLocale/CurrencyLocaleListBuilderUnitTest.php
+++ currency/tests/src/Entity/CurrencyLocale/CurrencyLocaleListBuilderUnitTest.php
@@ -5,7 +5,7 @@
  * Contains \Drupal\currency\Tests\Entity\CurrencyLocale\CurrencyLocaleListBuilderUnitTest.
  */
 
-namespace Drupal\currency\Tests\Entity\Currency;
+namespace Drupal\currency\Tests\Entity\CurrencyLocale;
 
 use Drupal\currency\Entity\CurrencyLocale\CurrencyLocaleListBuilder;
 use Drupal\Tests\UnitTestCase;
