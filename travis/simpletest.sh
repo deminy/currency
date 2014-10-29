4eee0ee9db997575d3f3b0c1be141d85c4d299e8
Bart Feenstra <bart@mynameisbart.com>
2014-10-29 21:34:40 +0100

    No longer execute Simpletest tests in Travis CI.

--- travis/simpletest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-php core/scripts/run-tests.sh --verbose --color --concurrency 4 --php `which php` --url http://localhost "Currency" "Currency Intl" | tee /tmp/test.txt
-# Simpletest does not exit with code 0 on success, so we will need to analyze
-# the output to ascertain whether the tests passed.
-TEST_EXIT=${PIPESTATUS[0]}
-TEST_SIMPLETEST=$(! egrep -i "([0-9]+ fails)|(PHP Fatal error)|([0-9]+ exceptions)" /tmp/test.txt > /dev/null)$?
-if [ $TEST_EXIT -eq 0 ] && [ $TEST_SIMPLETEST -eq 0 ]; then exit 0; else exit 1; fi
