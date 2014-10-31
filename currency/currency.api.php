e37aa701d31849eb5b809733a9b4760cd2e5ba59
Bart Feenstra <bart@mynameisbart.com>
2014-10-31 09:02:53 +0100

    Improved hook documentation.

--- currency/currency.api.php
+++ currency/currency.api.php
@@ -17,3 +17,17 @@ function hook_currency_exchange_rate_provider_alter(array &$definitions) {
   // Replace an exchange rate provider plugin with another.
   $definitions['foo_plugin_id']['class'] = 'Drupal\foo\FooExchangeRateProvider';
 }
+
+/**
+ * Alters amount formatter plugins.
+ *
+ * @param array $definitions
+ *   Keys are plugin IDs. Values are plugin definitions.
+ */
+function hook_currency_amount_formatter_alter(array &$definitions) {
+  // Remove an amount formatter plugin.
+  unset($definitions['foo_plugin_id']);
+
+  // Replace an amount formatter plugin with another.
+  $definitions['foo_plugin_id']['class'] = 'Drupal\foo\FooAmountFormatter';
+}
