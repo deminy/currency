5964f51f75d9cd383b9c573e09e4e3de9fd81281
Bart Feenstra <bart@mynameisbart.com>
2015-08-21 23:39:57 +0200

    Make Travis CI run Simpletest, and all tests on PHP 7 and HHMV.

---
+++ travis-ci/travis-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e $DRUPAL_TI_DEBUG
+
+# Run PHPUnit tests and submit code coverage statistics.
+drupal_ti_ensure_drupal
+drupal_ti_ensure_module_linked
+cd $DRUPAL_TI_DRUPAL_DIR
+cd $DRUPAL_TI_MODULES_PATH
+cd $DRUPAL_TI_MODULE_NAME
+mkdir -p build/logs
+$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit -c ./phpunit.xml.dist --bootstrap $DRUPAL_TI_DRUPAL_DIR/core/tests/bootstrap.php --verbose --debug --coverage-clover ./build/logs/clover.xml || exit 1
+composer require satooshi/php-coveralls:0.6.*
+php ./vendor/bin/coveralls -v -c ./.coveralls.yml
