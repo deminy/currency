3b7b48383ee2b9a04302512754699dbda7b30a9e
giancarlosotelo <giancarlo.sotelo.lajo@gmail.com>
2015-11-05 10:45:10 +0100

    Update .travis.yml to use composer update instead of install

--- travis-ci/travis-before-script.sh
+++ travis-ci/travis-before-script.sh
@@ -25,7 +25,7 @@ drupal_ti_ensure_module_linked
 # Initialize composer_manager.
 php modules/composer_manager/scripts/init.php
 composer drupal-rebuild
-composer install --prefer-source -n --verbose
+composer update --prefer-source -n --verbose
 
 # Enable main module and submodules.
 drush en -y currency currency_intl
